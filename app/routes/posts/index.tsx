8969f92244b679adc553eecaf71be11814c5aef4
caesar003 <caesarmuksid@gmail.com>
2022-09-01 01:25:27 +0700

    Create an Admin User Hook using Remix to Verify Admin Users

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -2,6 +2,7 @@ import { Link, useLoaderData } from "@remix-run/react"
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { getPostListings } from "~/models/post.server";
+import { useOptionalAdminUser } from "~/utils";
 
 type LoaderData = {
     posts: Awaited<ReturnType<typeof getPostListings>>
@@ -16,13 +17,18 @@ export const loader: LoaderFunction = async()=>{
 
 export default function PostRoute(){
     const {posts} = useLoaderData() as LoaderData;
+    const adminuser = useOptionalAdminUser();
+   
     
     return(
         <main>
             <h1>Posts</h1>
-            <Link to="admin" className="text-red-600 underline">
-                Admin
-            </Link>
+            {adminuser?(
+                <Link to="admin" className="text-red-600 underline">
+                    Admin
+                </Link>
+            ): null}
+            
             <ul>
                 {posts.map((post) => (
                     <li key={post.slug}>
