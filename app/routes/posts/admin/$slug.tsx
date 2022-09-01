22daa63c75c3d5cf25cc96eb900298a5008add10
caesar003 <caesarmuksid@gmail.com>
2022-09-01 08:24:51 +0700

    Add Update Functionality to an Edit Post Page in Remix

--- app/routes/posts/admin/$slug.tsx
+++ app/routes/posts/admin/$slug.tsx
@@ -1,7 +1,7 @@
 import { Form, useActionData, useLoaderData, useTransition } from "@remix-run/react";
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { redirect, json} from "@remix-run/node";
-import { createPost, getPost } from "~/models/post.server";
+import { createPost, getPost, updatePost } from "~/models/post.server";
 import invariant from "tiny-invariant";
 import { requireAdminUser } from "~/session.server";
 
@@ -49,7 +49,7 @@ export const action: ActionFunction = async ({request, params}) => {
     if(params.slug === 'new'){
         await createPost({title, slug, markdown});
     } else {
-        // TODO: update post
+        await updatePost(params.slug, {title, slug, markdown});
     }
 
     return redirect('/posts/admin');
@@ -62,8 +62,10 @@ export default function NewPostRoute(){
     const data = useLoaderData();
     const errors = useActionData() as ActionData;
     const transition = useTransition();
-    const isCreating = Boolean(transition.submission);
+    const isCreating = transition.submission?.formData.get("intent") === "create";
+    const isUpdating = transition.submission?.formData.get("intent") === "update";
 
+    const isNewPost = !data.post;
     return (
         <Form method="post" key={data.post?.slug ?? 'new'}>
             <p>
@@ -98,11 +100,15 @@ export default function NewPostRoute(){
             </p>
 
             <p className="text-right">
-                <button type="submit"
+                <button 
+                type="submit"
+                name="intent"
+                value={isNewPost ? 'create' : 'update'}
                 className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
-                disabled={isCreating}
+                disabled={isCreating || isUpdating}
                 >
-                   {isCreating ? "Creating...": "Create Post"}
+                    {isNewPost ? (isCreating ? "Creating..." : "Create Post") : null}
+                   {isNewPost ? null : isUpdating ?  "Updating...": "Update"}
                 </button>
             </p>
         </Form>
